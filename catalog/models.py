4869b103a9c1fbeec54aee24aabe393c7618a8cb
Pia <basiccreation@yahoo.com>
2021-05-29 20:27:08 -0700

    add borrower

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse  # generate URLs by reversing URL patterns
 import uuid  # Required for unique book instances
+from django.contrib.auth.models import User
 
 
 class Genre(models.Model):
@@ -79,6 +80,7 @@ class BookInstance(models.Model):
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True,
                                 blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
