a27de8594c02c22938024257331dba7959d666a2
Pia <basiccreation@yahoo.com>
2021-05-29 18:32:00 -0700

    add page count

--- catalog/views.py
+++ catalog/views.py
@@ -19,6 +19,10 @@ def index(request):
     num_genres = Genre.objects.all().count()
     num_martian_book = Book.objects.filter(title__contains='Martian').count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 10)
+    request.session['visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -26,6 +30,7 @@ def index(request):
         'num_authors': num_authors,
         'num_genres': num_genres,
         'num_martian_book': num_martian_book,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
