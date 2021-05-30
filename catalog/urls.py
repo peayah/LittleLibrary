34ad153d17858054e0037851afa7173eea8bdbd9
Pia <basiccreation@yahoo.com>
2021-05-30 00:09:55 -0700

    add filter so only librian can see borrowed books

--- catalog/urls.py
+++ catalog/urls.py
@@ -22,10 +22,16 @@ urlpatterns = [
          views.AuthorDetailView.as_view(),
          name='author-detail'),
 
-    path('mybooks/', views.LoanedBooksByUserListView.as_view(),
+    path('mybooks/',
+         views.LoanedBooksByUserListView.as_view(),
          name='my-borrowed'),
 
-    path('borrowedbooks/', views.LoanedBooksListView.as_view(),
+    path('borrowedbooks/',
+         views.LoanedBooksListView.as_view(),
          name='all-borrowed'),
+
+    path('book/<uuid:pk>/renew/',
+         views.renew_book_librarian,
+         name='renew-book-librarian'),
 
 ]
