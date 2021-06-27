48bc23c377183b419c379d8ce582873288390ed5
Pia <basiccreation@yahoo.com>
2021-06-26 23:03:44 -0700

    add whitenoise

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -156,3 +156,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
